a7780a12a801cf4d4b442f7a1e9a16671695cbc3
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-12 12:32:38 +0530

    attached the link of the dynamic elements to the elements card

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 import TestimonialCard from "@/components/TestimonialCard";
-import { userTestimonyType } from "@/components/types";
 import { userTestimonials } from "@/db/db";
 import { useEffect } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    console.log(userTestimonials);
-  }, []);
   return (
     <div className="max-w-screen p-8">
       <section className="grid gap-4 grid-cols-4">
-        {userTestimonials.map(({ name, country, profession }) => (
+        {userTestimonials.map(({ id, name, country, profession }) => (
           <TestimonialCard
             name={name}
             country={country}
             profession={profession}
-            cardURL="/"
+            cardURL={`/testimonials/${id}`}
           />
         ))}
       </section>
